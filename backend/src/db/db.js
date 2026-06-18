825bb4b39a0257e61c2f17c86dc87fe13660f8cd
Meenakshi Jha <meenjha1234@gmail.com>
2026-06-18 16:24:31 +0530

    Configure Neon database

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,10 +2,17 @@ import pkg from "pg";
 
 const {Pool}=pkg;
 
-export const pool=new Pool({
-    user:"postgres",
-    host:"localhost",
-    database:"school_management",
-    password:"123456",
-    port:5433,
-})
+// export const pool=new Pool({
+//     user:"postgres",
+//     host:"localhost",
+//     database:"school_management",
+//     password:"123456",
+//     port:5433,
+// })
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
